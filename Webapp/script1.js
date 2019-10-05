eb9c8df97fc7849321f7639b49b6366fc19f8389
Georg <55636135+gauge19@users.noreply.github.com>
2019-10-06 00:15:26 +0200

    playing around with script1.js as well as main.html. no actual progress.

--- Webapp/script1.js
+++ Webapp/script1.js
@@ -1,4 +1,34 @@
-var x = 10;
-if (x > 9){
-  document.write(x);
+function randint(max){
+  return Math.floor(Math.random() * Math.floor(max));
 }
+
+function car(name, color, year, maxspeed) {
+  this.name = name;
+  this.color = color;
+  this.age = 2019-year;
+  this.maxspeed = maxspeed;
+
+  this.get_name = function (){
+    return this.name;
+  }
+  this.set_name = function (name) {
+    this.name = name;
+  }
+}
+
+var x = randint(10);
+if (x > 5){
+  document.write("x is greater than 5, x: " + x);
+} else {
+  document.write("x is smaller than 5, x: " + x);
+}
+
+document.write("<br />")
+
+var car1 = new car ("Corolla", "green", 1974, randint(250));
+document.write(car1.age + ", " + car1.color + " " + car1.name + ", Topspeed: " + car1.maxspeed);
+
+document.write("<br />")
+
+car1.set_name("Toyota Corolla");
+document.write(car1.get_name());
