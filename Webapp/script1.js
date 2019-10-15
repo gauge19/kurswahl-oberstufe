e27dadeb123a84e89c1f6d08ba8e7b72f0cfda40
Georg <55636135+gauge19@users.noreply.github.com>
2019-10-15 17:28:07 +0200

    deleted unnecessary and experimental code

--- Webapp/script1.js
+++ Webapp/script1.js
@@ -2,39 +2,8 @@ function randint(max){
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function car(name, color, year, maxspeed) {
-  this.name = name;
-  this.color = color;
-  this.age = 2019-year;
-  this.maxspeed = maxspeed;
-
-  this.get_name = function (){
-    return this.name;
-  }
-  this.set_name = function (name) {
-    this.name = name;
-  }
-}
-
 function MyFunc() {
   var h1 = document.getElementById("heading1");
   h1.style.color = "blue";
   h1.innerHTML = "CLICK MOTHAFUCKA!";
 }
-
-var x = randint(10);
-if (x > 5){
-  document.write("x is greater than 5, x: " + x);
-} else {
-  document.write("x is smaller than 5, x: " + x);
-}
-
-document.write("<br />")
-
-var car1 = new car ("Corolla", "green", 1974, randint(250));
-document.write(car1.age + ", " + car1.color + " " + car1.name + ", Topspeed: " + car1.maxspeed);
-
-document.write("<br />")
-
-car1.set_name("Toyota Corolla");
-document.write(car1.get_name());
